23c83ecfd2ba16b8f37ee74746ac9e69565cd508
aj-cpp <ajujun@cpp.edu>
2022-10-05 19:27:12 -0700

    Initial commit

---
+++ tictactoe.c
@@ -0,0 +1,24 @@
+/*
+  TicTacToe
+  A game between 2 players or player vs. computer
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+  // TODO: Create multidimensional array to hold the player values
+
+  // TODO: Create a function for initGame
+  // initGame:
+  // - Display option to choose between vs. player or vs. computer
+
+  // TODO: Create a function to display the board
+
+  // TODO: Create game logic for TicTacToe
+
+
+  return 0;
+}
